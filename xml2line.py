6f87cdcf2cb47570f98e0f85c6456c51a48d30c8
James Tauber <jtauber@jtauber.com>
2018-08-05 14:49:06 -0400

    updated homer to include book ref

--- xml2line.py
+++ xml2line.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
+import re
+
 from lxml import etree
 
-with open("iliad.xml") as f:
+# filename = "iliad.xml"
+# subtype = "Book"
+# prefix = "Il"
+
+filename = "odyssey.xml"
+subtype = "book"
+prefix = "Od"
+
+with open(filename) as f:
     tree = etree.parse(f)
-    for book in tree.xpath("//TEI:div[@subtype='Book']", namespaces={"TEI": "http://www.tei-c.org/ns/1.0"}):
+    for book in tree.xpath(f"//TEI:div[@subtype='{subtype}']", namespaces={"TEI": "http://www.tei-c.org/ns/1.0"}):
         book_num = book.attrib["n"]
         for line in book.xpath(".//TEI:l", namespaces={"TEI": "http://www.tei-c.org/ns/1.0"}):
             line_num = line.attrib["n"]
-            text = line.xpath("string()").strip()
-            print(f"{book_num}.{line_num} {text}")
+            text = re.sub("\s+", " ", line.xpath("string()").strip())
+            print(f"{prefix}.{book_num}.{line_num} {text}")
