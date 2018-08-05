4af0ffe76eb04df416a055a604a8cfc3c6963679
James Tauber <jtauber@jtauber.com>
2018-08-05 00:19:22 -0400

    further README and ngram2 improvements

--- ngram2.py
+++ ngram2.py
@@ -7,10 +7,11 @@ def normalise(w):
     return w.strip("·.,")
 
 
-def ngram(it, n, norm_func=None):
+def ngram(it, n, norm_func=None, pad=False):
     """
     generates the n-grams (for given `n`) for a given iterator `it`, calling
     the optional given `norm_func` function on each item as well.
+    If `pad` is true the first n-1 and last n-1 n-grams will be padded.
     """
     if norm_func is None:
         norm_func = lambda w: w  # identity function
@@ -19,12 +20,13 @@ def ngram(it, n, norm_func=None):
 
     for current in it:
         window = window[1:] + (norm_func(current),)
-        if "*" not in window:
+        if pad or "*" not in window:
             yield window
 
-    # for i in range(n):
-    #     window = window[1:] + ("*",)
-    #     yield window
+    if pad:
+        for i in range(n):
+            window = window[1:] + ("*",)
+            yield window
 
 
 def summarise_counter(c):
@@ -36,23 +38,23 @@ def summarise_counter(c):
 ngrams = {}
 subgrams = {}
 
-# filename = "test2.txt"
-# s = 1
-# e = 5
+filename = "test.txt"
+s = 2
+e = 5
 
-filename = "iliad.txt"
-s = 4
-e = 74
+# filename = "iliad.txt"
+# s = 4
+# e = 74
 
 for N in range(e, s - 1, -1):
     with open(filename) as f:
         ngrams[N] = collections.Counter(ngram(f.read().split(), N, normalise))
         print(N)
         subgrams[N - 1] = collections.Counter()
-        for X, c in ngrams[N].items():
-            d = subgrams.get(N, {}).get(X, 0)
-            if c > 1 and (d == 0 or c > d):
-                print("   ", c, d, "|", " ".join(X))
-            if c > 1:
+        for X, naive_count in ngrams[N].items():
+            subgram_count = subgrams.get(N, {}).get(X, 0)
+            if naive_count > 1 and naive_count > subgram_count:
+                print("   ", naive_count, subgram_count, "|", " ".join(X))
+            if naive_count > 1:
                 for Y in ngram(X, N - 1):
-                    subgrams[N - 1][Y] += c
+                    subgrams[N - 1][Y] += naive_count
